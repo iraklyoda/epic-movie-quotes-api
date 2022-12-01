b094f2415d4f4a19c35a1ad611a3ba70733514d6
iraklyoda <iraklyoda42@gmail.com>
2022-12-01 16:04:20 +0400

    feat: create echo

--- app/Http/Controllers/QuoteCommentsController.php
+++ app/Http/Controllers/QuoteCommentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\AddCommentEvent;
 use App\Http\Requests\StoreQuoteCommentRequest;
 use App\Models\Comment;
 use App\Models\Quote;
@@ -10,10 +11,12 @@ class QuoteCommentsController extends Controller
 {
 	public function create(storeQuoteCommentRequest $request, Quote $quote)
 	{
-		Comment::create([
+		$comment = [
 			'user_id'  => jwtUser()->id,
 			'body'     => $request->body,
 			'quote_id' => $quote->id,
-		]);
+		];
+		event(new AddCommentEvent($comment));
+		Comment::create($comment);
 	}
 }
