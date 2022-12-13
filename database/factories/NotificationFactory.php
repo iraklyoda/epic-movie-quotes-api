3588272f54469f7143b78d08bee44be741cac195
iraklyoda <iraklyoda42@gmail.com>
2022-12-13 21:17:20 +0400

    feat: create docs

--- database/factories/NotificationFactory.php
+++ database/factories/NotificationFactory.php
@@ -9,15 +9,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class NotificationFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition()
+	{
+		return [
+		];
+	}
 }
